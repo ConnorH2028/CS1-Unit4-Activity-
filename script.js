7c03613548012f5b7d99c1414564149307d4b684
ConnorH2028 <connor.hamilton2028@gbwl.org>
2026-05-05 13:39:00 +0000

    querySelector

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+// Crate a boolean variable to track the mode
+let darMode = false;
+
+/* NEW JS FUNCTION: document.querrySelector()
+    lets you select an HTML lement and store
+    the reference in a variable for JS to use
+*/
+
+const body = document.querySelector("body");
+const heading = document.querySelector("#main-heading");
+const description = document.querySelector("#description");
+const toggleBtn = document.querySelector("#toggle");
+// CONST is like LET, but the value won't change
+
+// JS can now change attributes & properties!
+heading.textContent = "Light & Dark Website"; // JS wrote HTML code!
+heading.style.color = "white"; // JS wrote CSS code!
+
